d47fdd858bfa2fd39b148a47cf63f8c9b1827466
CJ1415 <connorjskinner@outlook.com>
2024-10-14 15:56:33 +0100

    Initial Message

--- my_first_code.py
+++ my_first_code.py
@@ -1,40 +1,40 @@
-# print("My first code")
+print("My first code")
 
-# print("Hello", end="")
-# print("World")
+print("Hello", end="")
+print("World")
 
-# name="Connor"
-# age = 20 
-# print(f"{name} is {age} years old")
+name="Connor"
+age = 20 
+print(f"{name} is {age} years old")
 
-# print("Hello " +  name)
-# print("{} is {} years old".format("Alice", 30))
+print("Hello " +  name)
+print("{} is {} years old".format("Alice", 30))
 
-# print(342+699)
+print(342+699)
 
-# print(5022 - 901)
-# print(5 * 10)
-# print(604 / 4)
-# print( 3 % 2)
-# print(10 % 4)
-# print(9 // 3)
-# print(8 // 3)
-# print(5 ** 2)
-# print(4 ** 3)
+print(5022 - 901)
+print(5 * 10)
+print(604 / 4)
+print( 3 % 2)
+print(10 % 4)
+print(9 // 3)
+print(8 // 3)
+print(5 ** 2)
+print(4 ** 3)
 
-# a = 265
-# b = 5924
-# result = a + b
-# print("a + b = ", result)
+a = 265
+b = 5924
+result = a + b
+print("a + b = ", result)
 
-# (6 * 4) + (5**2)-(4%3)
+(6 * 4) + (5**2)-(4%3)
 
-# a = (6*4)
-# b = (5 **2)
-# c = (4%3)
-# result = a + b - c
-# print("a + b - c", result)
-# print(b)
+a = (6*4)
+b = (5 **2)
+c = (4%3)
+result = a + b - c
+print("a + b - c", result)
+print(b)
 
 def sum(a, b):
     c = a + b
@@ -42,3 +42,8 @@ def sum(a, b):
 
 x = sum(2, 3)
 print(x)
+
+a = 141
+b = 188274
+
+print(a*b) 
